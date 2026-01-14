2f64319c4680497f2d612445233ce7d5ea228b20
Insydian <cr.larue03@gmail.com>
2026-01-13 22:59:13 -0600

    completed all modules. Added reactivity with preact. Added dark mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 export default defineConfig({
+  site: "https://insydian.netlify.app",
+
   markdown: {
     syntaxHighlight: 'shiki',
     shikiConfig: {
       theme: 'github-dark',
     },
   },
+
+  integrations: [preact()],
 });
 // https://astro.build/config
 //export default defineConfig({});
